32ff7baa7ca07834c9186c58ffffb2d58620c2be
VishalMaliHDS <vishal.mali@hitachids.com>
2026-06-11 23:43:30 +0530

    Simplify stop_container.sh and add greeting
    Removed container stopping logic and added a greeting.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,15 +1,5 @@
-
 #!/bin/bash
 set -e
 
-echo "Running stop_container.sh"
-docker ps
-
-containers=$(docker ps -q)
-echo "Containers: $containers"
-
-if [ -n "$containers" ]; then
-  docker rm -f $containers
-else
-  echo "No running containers found"
-fi
+# Stop the running container (if any)
+echo "Hi"
